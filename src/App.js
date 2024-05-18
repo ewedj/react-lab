fb315570862581135db4adb1b876d8092d6befba
Ewelina Dubiel-Jaros <ewelinadubiel92@gmail.com>
2024-05-18 15:17:03 +0200

    milligram

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 import {useState} from "react";
+import "milligram";
 
 export default function App() {
   // let email:string = 'ewelinadubiel92@gmail.com';
   const [email, setEmail] = useState('ewelinadubiel92@gmail.com');
+  const [isAutenticated, setIsAutenticated] = useState(false);
 
   function handleChange(event) {
     setEmail(event.target.value);
   }
 
-
-  let message = <div>Masz krótki adres</div>;
-  if (email.length > 15) {
-    message = <div>Masz bardzo długi adres</div>
-  }else if (email.length > 5) {
-    message = <div>Masz w porządku adres</div>
+  let content;
+  if (isAutenticated) {
+    content = <div>
+      <h2>Zaloguj się e-mailem</h2>
+      <button type="button" onClick={() => alert(email)}> Wyloguj się</button>
+    </div>
+  }else {
+    content = <div>
+      <input type="text" onChange={handleChange}/>
+      <button type="button" onClick={() => setIsAutenticated(true)}>
+        Zaloguj się</button>
+    </div>
   }
 
-  let header1 = <h1>System do zapisów na zajęcia</h1>
-  let header2 = <h2>Twój email to: {email}</h2>
+
+  // let message = <div>Witaj{email}</div>;
+  let header1 = <h1>Witaj w systemie do zapisów na zajęcia</h1>
+  // let header2 = <h2>Zaloguj się e-mailem</h2>
 
   return (
     <div>
         {header1}
-        {header2}
-        {message}
-      <input type="text" onChange={handleChange}/> <button type="button" onClick={() => alert(email)}>
-      Wyświetl mój email w alercie</button>
-
+        {content}
     </div>
   );
 }
